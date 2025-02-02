a0882a6d388b4292705c58ba447cd61c9b651a98
Akai Mansur <forunivese2318@gmail.com>
2025-02-02 20:03:59 +0500

    added payment

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import bookingRoutes from './routes/booking.js';
-import authRoutes from './routes/auth.js';
-import connectDB from './db.js';
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import bookingRoutes from "./routes/booking.js";
+import authRoutes from "./routes/auth.js";
+import connectDB from "./db.js";
+import paymentRoutes from "./routes/payment.js";
 
 // Подключение базы данных
 connectDB();
-
-console.log(`🟢 Загруженный SECRET_KEY: ${process.env.JWT_SECRET}`);
-
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -26,32 +24,35 @@ app.use(express.json());
 app.use(cors()); // Разрешает запросы со всех доменов
 
 // Указываем папку public для статических файлов
-const publicDir = path.join(__dirname, 'public');
+const publicDir = path.join(__dirname, "public");
 app.use(express.static(publicDir));
 
 // Маршруты для бронирования
-app.use('/booking', bookingRoutes);
+app.use("/booking", bookingRoutes);
 
 // Маршруты для авторизации
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
+
+// Payment routes
+app.use("/payment", paymentRoutes);
 
 // Главная страница
-app.get('/', (req, res) => {
-    res.sendFile(path.join(publicDir, 'home.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(publicDir, "home.html"));
 });
 
 // Маршруты для HTML-страниц без .html
-app.get('/:page', (req, res, next) => {
-    const { page } = req.params;
-    const filePath = path.join(publicDir, `${page}.html`);
-    res.sendFile(filePath, (err) => {
-        if (err) {
-            next(); // Передаем обработку дальше, если файл не найден
-        }
-    });
+app.get("/:page", (req, res, next) => {
+  const { page } = req.params;
+  const filePath = path.join(publicDir, `${page}.html`);
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      next(); // Передаем обработку дальше, если файл не найден
+    }
+  });
 });
 
 // Запуск сервера
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
